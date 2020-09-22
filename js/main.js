2abdaa1a82cbbc17323eea27655a3c7e4cfdb381
okapeeokapee <okapee420@gmail.com>
2020-09-22 23:58:52 +0900

    arrow function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
 'use strict';
 
-const sum = function( a, b ,c ){
-  // console.log(a + b + c);
-  return a + b + c;
-};
+const double = a => a * 2;
 
-sum(1,2,3);
-sum(3,4,5);
-
-const total = sum(1,2,3) + sum(3,4,5);
-console.log(total);
+console.log(double(12));
 
