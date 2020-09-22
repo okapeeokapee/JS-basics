ce970c218f551bb674197170840a8246d26f2180
okapeeokapee <okapee420@gmail.com>
2020-09-22 17:19:15 +0900

    `${message}`

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 'use strict';
 
-function showAd() {
+function showAd(message ='Ad' ) {
   console.log('--------------');
-  console.log('-----ad-----');
+  console.log(`-----${message}-----`);
   console.log('--------------');
 }
 
+showAd('Header Ad');
+console.log('Tom is great!');
+console.log('Tom is great!');
+console.log('Tom is great!');
+// showAd('Ad');
 showAd();
 console.log('Tom is great!');
 console.log('Tom is great!');
 console.log('Tom is great!');
-showAd();
-console.log('Tom is great!');
-console.log('Tom is great!');
-console.log('Tom is great!');
-showAd();
+showAd('Footer Ad');
 console.log('Tom is great!');
 console.log('Tom is great!');
 
