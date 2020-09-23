64eb84f7582ddab6e29c1610ea2c0d2a4e137d19
okapeeokapee <okapee420@gmail.com>
2020-09-23 22:06:29 +0900

    classList.add('circle'); .box transition:.8s; .circle transform:rotate(360deg);

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
 
 {
   document.getElementById('target').addEventListener('click', () => {
-    document.getElementById('target').style.background = 'pink';
-    document.getElementById('target').style.borderRadius = '50%';
+    // document.getElementById('target').style.background = 'pink';
+    // document.getElementById('target').style.borderRadius = '50%';
+    document.getElementById('target').classList.add('circle');
   });
 }
 
