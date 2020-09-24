931991598ca31ab0be12af8eed89ac7c701491b7
okapeeokapee <okapee420@gmail.com>
2020-09-24 15:04:19 +0900

    const target classList.toggle

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 'use strict';
-
 {
-  document.getElementById('target').addEventListener('click', () => {
-    // document.getElementById('target').style.background = 'pink';
-    // document.getElementById('target').style.borderRadius = '50%';
-    document.getElementById('target').classList.add('circle');
+  const target = document.getElementById('target');
+
+  target.addEventListener('click', () => {
+  target.classList.toggle('circle');
   });
 }
 
