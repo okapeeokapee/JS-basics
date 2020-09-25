260b308b6e4f5bd5d089d24f579a747101aad1e5
okapeeokapee <okapee420@gmail.com>
2020-09-25 22:57:46 +0900

    complete

--- js/main.js
+++ js/main.js
@@ -1,18 +1,2 @@
-'use strict';{
-  const box = document.getElementById('box');
-  box.classList.add('box');
-
-  const btn = document.getElementById('btn');
-  btn.classList.add('btn');
-
-  btn.addEventListener('click', () =>{
-    const num = 10
-    for(let i = 0; i<num; i++){
-       const choice = Math.floor(Math.random() * )
-      box.textContent = num;
-    }
-  
-  });
-}
-
+'
 
